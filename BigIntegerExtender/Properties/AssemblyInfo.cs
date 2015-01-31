d9fb322e5decb59ce1cef969597331b521d6ee33
Davide Riva <driva95@gmail.com>
2015-01-31 11:35:47 +0100

    Version changed

--- BigIntegerExtender/Properties/AssemblyInfo.cs
+++ BigIntegerExtender/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
+ 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("af54bffe-4925-40a8-a008-4d6bd1414764")]
 
